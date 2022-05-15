f599190babe07bf687f8ea215420181557c7ab5b
s9hn <tnghqn@naver.com>
2022-05-16 06:10:44 +0900

    [Chore] 기초세팅 중
    1. manifest 수정
    2. theme 수정
    3. color 수정
    4. string 수정
    5. build gradle 수정

---
+++ app/src/main/java/sopt/org/joint15_29cm/feature/mino/ReadActivity.kt
@@ -0,0 +1,13 @@
+package sopt.org.joint15_29cm.feature.mino
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import sopt.org.joint15_29cm.R
+
+class ReadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read)
+    }
+}
---
+++ app/src/main/java/sopt/org/joint15_29cm/feature/zyoung/CreateActivity.kt
@@ -0,0 +1,12 @@
+package sopt.org.joint15_29cm.feature.zyoung
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import sopt.org.joint15_29cm.R
+
+class CreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create)
+    }
+}
