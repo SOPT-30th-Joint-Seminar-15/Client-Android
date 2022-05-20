13b65b48aed1d586d71d1dfd3e07ec7fb2beb903
wlwpfh <0112jyoung@naver.com>
2022-05-20 16:02:45 +0900

    [Feat] radiobutton 체크 설정하기

--- app/src/main/java/sopt/org/joint15_29cm/feature/zyoung/CreateActivity.kt
+++ app/src/main/java/sopt/org/joint15_29cm/feature/zyoung/CreateActivity.kt
@@ -1,7 +1,12 @@
 package sopt.org.joint15_29cm.feature.zyoung
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import sopt.org.joint15_29cm.R
 import sopt.org.joint15_29cm.databinding.ActivityCreateBinding
 import sopt.org.joint15_29cm.util.CustomDialog
@@ -14,12 +19,50 @@ class CreateActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initDialog()
+        initControlRadioButtons()
     }
 
     private fun initDialog() {
         binding.btnCreateRegister.setOnClickListener {
             val dialog = CustomDialog(this)
             dialog.showCreateDialog(R.layout.dialog_create)
+
+        }
+    }
+
+    private fun initControlRadioButtons() {
+        val radioButtonList = listOf(binding.radiobuttonCreateDelivery,binding.radiobuttonCreateOrder,binding.radiobuttonCreateCancel,
+            binding.radiobuttonCreateRefund,
+            binding.radiobuttonCreateReturn,binding.radiobuttonCreateExchange,binding.radiobuttonCreateGift,binding.radiobuttonCreateDeposit,
+            binding.radiobuttonCreateChosen,binding.radiobuttonCreateCoupon,
+            binding.radiobuttonCreateEtc, binding.radiobuttonCreateItem,
+            binding.radiobuttonCreateDocument,
+
+        binding.radiobuttonCreateSystem,
+        binding.radiobuttonCreateUserinfo,
+        binding.radiobuttonCreateUserrule,
+
+        binding.radiobuttonCreatePurchaseinfo)
+
+
+        for(i in 0 until radioButtonList.size)
+        radioButtonList[i].setOnCheckedChangeListener { _, isChecked ->
+            if(radioButtonList[i].isChecked){
+                if(i in 0..7)
+                    binding.constraintlayoutCreateOrderinfo.visibility= View.VISIBLE
+                else
+                    binding.constraintlayoutCreateOrderinfo.visibility=View.GONE
+                //하나가 체크되면 나머지 다 체크 풀기
+                for(x in 0 until i){
+                    radioButtonList[x].isChecked=false
+
+                }
+                for(x in i+1 until radioButtonList.size){
+                    radioButtonList[x].isChecked=false
+
+                }
+            }
+
         }
     }
 }
